462c3cdf0571299b3287a8dbfb4f3e5ac491a199
Otavio <otaviopalma@gmail.com>
2019-05-31 17:24:07 -0300

    Update: imagens

--- index.php
+++ index.php
@@ -1,72 +1,68 @@
 <?php
-    require_once 'config.php';
-    include(HEADER_TEMPLATE);
+require_once 'config.php';
+include(HEADER_TEMPLATE);
 ?>
 
 <!-- stylesheets import start -->
 
+<link rel="stylesheet" href="css/border.css">
 
 <!-- stylesheets import start -->
 
 <br><br>
 
-<img src="img/blue.png" id="img00" width="50" height="50"/>
-<img src="img/blue.png" id="img01" width="50" height="50"/>
-<img src="img/blue.png" id="img02" width="50" height="50"/>
-<img src="img/blue.png" id="img03" width="50" height="50"/>
-<img src="img/blue.png" id="img04" width="50" height="50"/>
-<img src="img/blue.png" id="img05" width="50" height="50"/>
-
-<br>
-
-<img src="img/blue.png" id="img10" width="50" height="50"/>
-<img src="img/blue.png" id="img11" width="50" height="50"/>
-<img src="img/blue.png" id="img12" width="50" height="50"/>
-<img src="img/blue.png" id="img13" width="50" height="50"/>
-<img src="img/blue.png" id="img14" width="50" height="50"/>
-<img src="img/blue.png" id="img15" width="50" height="50"/>
-
-<br>
-
-<img src="img/blue.png" id="img20" width="50" height="50"/>
-<img src="img/blue.png" id="img21" width="50" height="50"/>
-<img src="img/blue.png" id="img22" width="50" height="50"/>
-<img src="img/blue.png" id="img23" width="50" height="50"/>
-<img src="img/blue.png" id="img24" width="50" height="50"/>
-<img src="img/blue.png" id="img25" width="50" height="50"/>
-
-<br>
-
-<img src="img/blue.png" id="img30" width="50" height="50"/>
-<img src="img/blue.png" id="img31" width="50" height="50"/>
-<img src="img/blue.png" id="img32" width="50" height="50"/>
-<img src="img/blue.png" id="img33" width="50" height="50"/>
-<img src="img/blue.png" id="img34" width="50" height="50"/>
-<img src="img/blue.png" id="img35" width="50" height="50"/>
-
-<br>
-
-<img src="img/blue.png" id="img40" width="50" height="50"/>
-<img src="img/blue.png" id="img41" width="50" height="50"/>
-<img src="img/blue.png" id="img42" width="50" height="50"/>
-<img src="img/blue.png" id="img43" width="50" height="50"/>
-<img src="img/blue.png" id="img44" width="50" height="50"/>
-<img src="img/blue.png" id="img45" width="50" height="50"/>
-
-<br>
-
-<img src="img/blue.png" id="img50" width="50" height="50"/>
-<img src="img/blue.png" id="img51" width="50" height="50"/>
-<img src="img/blue.png" id="img52" width="50" height="50"/>
-<img src="img/blue.png" id="img53" width="50" height="50"/>
-<img src="img/blue.png" id="img54" width="50" height="50"/>
-<img src="img/blue.png" id="img55" width="50" height="50"/>
-
-<br>
-
+<div class="offset-3 col-6">
+    <div  id="board" class="offset-md-1 offset-2">
+        <div class="pt-4 col-2 pr-1">
+            <div class="pt-2">
+                <img src="img/blue.png" id="img00" />
+            </div>
+        </div>
+        <img src="img/blue.png" id="img01" />
+        <img src="img/blue.png" id="img02" />
+        <img src="img/blue.png" id="img03" />
+        <img src="img/blue.png" id="img04" />
+        <img src="img/blue.png" id="img05" />
+        <img src="img/red.png" id="img10" />
+        <img src="img/red.png" id="img11" />
+        <img src="img/red.png" id="img12" />
+        <img src="img/red.png" id="img13" />
+        <img src="img/red.png" id="img14" />
+        <img src="img/red.png" id="img15" />
+        <br><br>
+        <img src="img/blue.png" id="img20" />
+        <img src="img/blue.png" id="img21" />
+        <img src="img/blue.png" id="img22" />
+        <img src="img/blue.png" id="img23" />
+        <img src="img/blue.png" id="img24" />
+        <img src="img/blue.png" id="img25" />
+        <br><br>
+        <img src="img/blue.png" id="img30" />
+        <img src="img/blue.png" id="img31" />
+        <img src="img/blue.png" id="img32" />
+        <img src="img/blue.png" id="img33" />
+        <img src="img/blue.png" id="img34" />
+        <img src="img/blue.png" id="img35" />
+        <br><br>
+        <img src="img/blue.png" id="img40" />
+        <img src="img/blue.png" id="img41" />
+        <img src="img/blue.png" id="img42" />
+        <img src="img/blue.png" id="img43" />
+        <img src="img/blue.png" id="img44" />
+        <img src="img/blue.png" id="img45" />
+        <br><br>
+        <img src="img/blue.png" id="img50" />
+        <img src="img/blue.png" id="img51" />
+        <img src="img/blue.png" id="img52" />
+        <img src="img/blue.png" id="img53" />
+        <img src="img/blue.png" id="img54" />
+        <img src="img/blue.png" id="img55" />
+        <br><br>
+    </div>
+</div>
 
 <?php
-    include(FOOTER_TEMPLATE);
+include(FOOTER_TEMPLATE);
 ?>
 
 <!-- scripts import start -->
