b9d203bf25aada3f9cd91e4c56cf8cb2ce336621
Otavio <otaviopalma@gmail.com>
2019-05-31 20:29:05 -0300

    Update: design peças finalizado

--- index.php
+++ index.php
@@ -10,54 +10,143 @@ include(HEADER_TEMPLATE);
 <!-- stylesheets import start -->
 
 <br><br>
-
-<div class="offset-3 col-6">
-    <div  id="board" class="offset-md-1 offset-2">
-        <div class="pt-4 col-2 pr-1">
-            <div class="pt-2">
-                <img src="img/blue.png" id="img00" />
+<div class="container">
+    <div class="row">
+        <div class="col">
+        </div>
+        <div class="col pt-5">
+            <img src="img/ai.png">
+        </div>
+        <div class="col-7">
+            <div id="board" class="pt-5 pl-4">
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3">
+                        <img src="img/blue.png" id="img00" />
+                    </div>
+                    <div class="pl-2">
+                        <img src="img/blue.png" id="img01" />
+                    </div>
+                    <div class="pl-2">
+                        <img src="img/blue.png" id="img02" />
+                    </div>
+                    <div class="pl-1">
+                        <img src="img/blue.png" id="img03" />
+                    </div>
+                    <div class="pl-1">
+                        <img src="img/blue.png" id="img04" />
+                    </div>
+                    <div class="pl-2">
+                        <img src="img/blue.png" id="img05" />
+                    </div>
+                </div>
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3 pt-1">
+                        <img src="img/blue.png" id="img10" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img11" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img12" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img13" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img14" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img15" />
+                    </div>
+                </div>
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3 pt-1">
+                        <img src="img/blue.png" id="img20" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img21" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img22" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img23" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img24" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img25" />
+                    </div>
+                </div>
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3 pt-1">
+                        <img src="img/blue.png" id="img30" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img31" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img32" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img33" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img34" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img35" />
+                    </div>
+                </div>
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3 pt-2">
+                        <img src="img/blue.png" id="img40" />
+                    </div>
+                    <div class="pt-2 pl-2">
+                        <img src="img/blue.png" id="img41" />
+                    </div>
+                    <div class="pt-2 pl-2">
+                        <img src="img/blue.png" id="img42" />
+                    </div>
+                    <div class="pt-2 pl-1">
+                        <img src="img/blue.png" id="img43" />
+                    </div>
+                    <div class="pt-2 pl-1">
+                        <img src="img/blue.png" id="img44" />
+                    </div>
+                    <div class="pt-2 pl-2">
+                        <img src="img/blue.png" id="img45" />
+                    </div>
+                </div>
+                <div class="row pt-3 ml-4">
+                    <div class="pl-3 pt-1">
+                        <img src="img/blue.png" id="img50" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img51" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img52" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img53" />
+                    </div>
+                    <div class="pt-1 pl-1">
+                        <img src="img/blue.png" id="img54" />
+                    </div>
+                    <div class="pt-1 pl-2">
+                        <img src="img/blue.png" id="img55" />
+                    </div>
+                </div>
+                <br> <br> <br>
             </div>
         </div>
-        <img src="img/blue.png" id="img01" />
-        <img src="img/blue.png" id="img02" />
-        <img src="img/blue.png" id="img03" />
-        <img src="img/blue.png" id="img04" />
-        <img src="img/blue.png" id="img05" />
-        <img src="img/red.png" id="img10" />
-        <img src="img/red.png" id="img11" />
-        <img src="img/red.png" id="img12" />
-        <img src="img/red.png" id="img13" />
-        <img src="img/red.png" id="img14" />
-        <img src="img/red.png" id="img15" />
-        <br><br>
-        <img src="img/blue.png" id="img20" />
-        <img src="img/blue.png" id="img21" />
-        <img src="img/blue.png" id="img22" />
-        <img src="img/blue.png" id="img23" />
-        <img src="img/blue.png" id="img24" />
-        <img src="img/blue.png" id="img25" />
-        <br><br>
-        <img src="img/blue.png" id="img30" />
-        <img src="img/blue.png" id="img31" />
-        <img src="img/blue.png" id="img32" />
-        <img src="img/blue.png" id="img33" />
-        <img src="img/blue.png" id="img34" />
-        <img src="img/blue.png" id="img35" />
-        <br><br>
-        <img src="img/blue.png" id="img40" />
-        <img src="img/blue.png" id="img41" />
-        <img src="img/blue.png" id="img42" />
-        <img src="img/blue.png" id="img43" />
-        <img src="img/blue.png" id="img44" />
-        <img src="img/blue.png" id="img45" />
-        <br><br>
-        <img src="img/blue.png" id="img50" />
-        <img src="img/blue.png" id="img51" />
-        <img src="img/blue.png" id="img52" />
-        <img src="img/blue.png" id="img53" />
-        <img src="img/blue.png" id="img54" />
-        <img src="img/blue.png" id="img55" />
-        <br><br>
+        <div class="col pt-5">
+            <img src="img/human.png">
+        </div>
+        <div class="col">
+        </div>
     </div>
 </div>
 
